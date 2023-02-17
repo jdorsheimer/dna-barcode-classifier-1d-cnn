4a36f6950d2acbdc8b4a47a2a9886d1439efd44d
Justin <justin.dorsheimer@gmail.com>
2023-02-16 21:11:57 -0700

    implemented base model class

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from utilities.tensor_utils import one_hot_encode_1d, to_tensor
-from utilities.model_utils import dna_barcode_CNN_1d
+# from utilities.tensor_utils import one_hot_encode_1d, to_tensor
+# from utilities.model_utils import dna_barcode_CNN_1d
 
-
-test_sequence = "ATTTGATGTTCATGAT"
+# test_sequence = "ATTTGATGTTCATGAT"
 # print(one_hot_encode_2d(test_sequence))
-print(to_tensor(one_hot_encode_1d(test_sequence)))
+# print(to_tensor(one_hot_encode_1d(test_sequence)))
 #print(to_tensor(one_hot_encode_1d(test_sequence)))
+
+
 
 
