27132b8d69d75866ee2d2c41667ebda44496dfd0
Justin Dorsheimer <66982778+jdorsheimer@users.noreply.github.com>
2023-03-05 13:10:27 -0700

    Update other_utils.py

--- utilities/other_utils.py
+++ utilities/other_utils.py
@@ -9,7 +9,7 @@ def unique_char_counts(df, col_name):
     seq_series = df[col_name]
     # Concatenate all sequences into a single string.
     all_seq_str = ''.join(seq_series)
-    # Count the occurrence of each character
+    # Count the occurrence of each character.
     char_counts = {}
     for char in all_seq_str:
         if char in char_counts:
